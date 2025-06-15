caf12f1ff6b00938e18ebd5f9d68a08b3c00e9c3
Mathias Toala <fetoala@outlook.com>
2025-06-14 22:00:55 -0500

    movement progress

--- game.ts
+++ game.ts
@@ -1,3 +1,17 @@
+interface Isnake{
+    width: number,
+    posx: number,
+    posy: number,
+    score: number
+}
+
+let snake: Isnake = {
+    width: 3,
+    posx: 10,
+    posy: 10,
+    score: 0
+}
+
 var keypress = require('keypress');
  
 // make `process.stdin` begin emitting "keypress" events
@@ -15,24 +29,57 @@ process.stdin.on('keypress', function (ch, key) {
   switch(key.name){
     case 'w':
     case 'up':
-        console.log('arriba');
+       // console.log('arriba');
+    //    snake.posx =0;
+       snake.posy -=1;
+       render();
         break;     
     case 'a':
     case 'left':
-        console.log('izquierda');
+        //console.log('izquierda');
+        snake.posx -=1;
+        //snake.posy =0;
+        render();
         break;
     case 's':
     case 'down':
-        console.log('abajo');
+        //console.log('abajo');
+        //snake.posx =0;
+        snake.posy +=1;
+        render();
         break;
     case 'd':
     case 'right':
-        console.log('derecha');
+        //console.log('derecha');
+        snake.posx +=1;
+        //snake.posy =0;
+        render();
         break;
   }
 });
 
+let lenght: number = 57;
+let ceiling: string = " ";
+for(let i = 0; i < lenght; i++){
+    ceiling+="*";
+}
 
+const render = function(){
+    console.log(ceiling);
+    for (let m = 0; m<10; m++){
+        let row = "|";
+        for(let n = 0; n<lenght; n++){
+            if(m == snake.posy && n == snake.posx){
+                row= row + "@"
+            }else{
+                row= row + "_"
+            }
+        }
+        row= row + "|"
+    console.log(row);
+    }
+    console.log(ceiling);
+}
  
 process.stdin.setRawMode(true);
 process.stdin.resume();
